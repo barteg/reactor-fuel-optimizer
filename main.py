4187111644fef67e8889d3339925248a9f8815f2
barteg <barteg@barteg.barteg>
2025-04-26 21:24:31 +0200

    Added fitness function

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
-# main.py
-
 from core_sim.core_grid import CoreGrid
-from visualization.plot_core import plot_core
+from optimization.fitness import fitness
 
 def main():
-    # Tworzenie rdzenia
     core = CoreGrid(size=20)
-
-    # Wyświetlanie kilku przykładowych FA
-    fas = core.get_all_fuel_assemblies()
-    sample_fas = [fa for fa in fas if fa.fa_type == "movable"][:5]
-
-    print("Sample Movable Fuel Assemblies:")
-    for fa in sample_fas:
-        print(fa)
-
-    # Rysowanie siatki
-    plot_core(core)
+    fit = fitness(core)
+    print(f"Fitness of initial core layout: {fit:.4f}")
 
 if __name__ == "__main__":
     main()
