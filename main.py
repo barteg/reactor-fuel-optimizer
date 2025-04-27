4180f4ce87525c63db6cbc4f79e7b5f61e20c988
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2025-04-27 18:47:53 +0200

    main

--- main.py
+++ main.py
@@ -1,1 +1,20 @@
+from core_sim.core_grid import CoreGrid
+from visualization.plot_core import plot_core
 
+def main():
+    # Tworzenie rdzenia
+    core = CoreGrid(size=20)
+
+    # Wyświetlanie kilku przykładowych FA
+    fas = core.get_all_fuel_assemblies()
+    sample_fas = [fa for fa in fas if fa.fa_type == "movable"][:5]
+
+    print("Sample Movable Fuel Assemblies:")
+    for fa in sample_fas:
+        print(fa)
+
+    # Rysowanie siatki
+    plot_core(core)
+
+if _name_ == "_main_":
+    main()
