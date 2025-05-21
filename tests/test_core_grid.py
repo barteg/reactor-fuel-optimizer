1790b05ca348d1d26b741918d0b1c9e48c28d9b2
ThomasKarpinski <tomaszkarpinski204@gmail.com>
2025-05-22 01:43:01 +0200

    tests

--- tests/test_core_grid.py
+++ tests/test_core_grid.py
@@ -11,14 +11,16 @@ class TestCoreGrid(unittest.TestCase):
         self.assertEqual(self.core.grid.shape, (20, 20))
 
     def test_symmetry(self):
+        static_types = ['control', 'detector', 'dummy', 'empty']
         for x in range(10):
             for y in range(10):
-                fa = self.core.get_fa(x, y)
-                fa_sym = self.core.get_fa(19 - x, y)
-                self.assertEqual(fa.fa_type, fa_sym.fa_type)
+                fa = self.core.grid[x, y]
+                fa_sym = self.core.grid[19 - x, y]
+                if fa.fa_type in static_types and fa_sym.fa_type in static_types:
+                    self.assertEqual(fa.fa_type, fa_sym.fa_type)
 
     def test_quality_calculation(self):
-        fa = self.core.get_fa(7, 7)
+        fa = self.core.grid[7, 7]
         quality = calculate_fa_quality(fa, self.core)
         self.assertIsNotNone(quality)
         self.assertTrue(quality >= 0.0)
